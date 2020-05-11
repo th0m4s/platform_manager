7f126c0a3038f8e809e1f7ec1a2c45df55e3b86a
Thomas LEDOS <thomas.ledos92@gmail.com>
2020-05-11 19:04:46 +0200

    Force DB name

--- server/database_server.js
+++ server/database_server.js
@@ -11,7 +11,7 @@ const knex = Knex({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        database: "platform_manager"
     }
 });
 
