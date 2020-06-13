6e2fc39423bcd5bec8e8abef51a5bcb803f44ae7
Ubuntu <ubuntu@vps-71d73292.vps.ovh.net>
2020-06-13 15:39:41 +0200

    Added https server and http redirection

--- pmng_server/public_web/http_public_server.js
+++ pmng_server/public_web/http_public_server.js
@@ -4,8 +4,20 @@ const logger = require("../platform_logger").logger();
 const privileges = require("../privileges");
 const intercom = require("../intercom/intercom_client").connect();
 
+const enable_https = process.env.ENABLE_HTTPS.toLowerCase() == "true";
+// same line as in web_server.js
+
 function start() {
-    let httpServer = http.createServer(web.webServe).listen(80, () => {
+    let serve = web.webServe;
+
+    if(enable_https) {
+        serve = (req, res) => {
+            res.writeHead(302, {Location: "https://" + req.headers.host + req.url});
+            res.end();
+        };
+    }
+
+    let httpServer = http.createServer(serve).listen(80, () => {
         logger.info("http public server started.");
 
         intercom.send("webStarted", {type: "http"});
--- pmng_server/public_web/https_public_server.js
+++ pmng_server/public_web/https_public_server.js
@@ -3,9 +3,32 @@ const web = require("../web_server");
 const logger = require("../platform_logger").logger();
 const privileges = require("../privileges");
 const intercom = require("../intercom/intercom_client").connect();
+const regex_utils = require("../regex_utils");
+const path = require("path");
+const greenlock_manager = require("../https/greenlock_manager");
 
 function start() {
-    const https_options = {};
+    const https_options = {
+        SNICallback: (serverName, cb) => {
+            regex_utils.testCustom(serverName).then((project) => {
+                let serverFile = serverName;
+                if(project == null) {
+                    serverFile = process.env.ROOT_DOMAIN;
+                } else {
+                    if(serverFile.startsWith("www.")) serverFile = serverFile.substring(4);
+                    // should be done for each subdomaines
+                    // subdomains are the same as in regex_utils
+                }
+
+                return greenlock_manager.getSecureContext(serverFile);
+            }).then((context) => {
+                cb(null, context);
+            }).catch((error) => {
+                logger.error("Cannot create secure context for domain " + serverName + ":" + error);
+                cb(error);
+            });
+        }
+    };
     
     https.createServer(https_options, web.webServe).listen(443, () => {
         logger.info("https public server started.");
