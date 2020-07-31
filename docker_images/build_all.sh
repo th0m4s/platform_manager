46eec5f000087e67488ddf7fa19adc008b593732
OriginalsByToto <thomas.ledos92@gmail.com>
2020-07-31 20:39:57 +0200

    Corrected mariadb docker image

--- docker_images/build_all.sh
+++ docker_images/build_all.sh
@@ -4,4 +4,4 @@ docker build -t pmng/node:13.14.0 ./node/v13
 docker build -t pmng/node:12.18.3 ./node/v12
 docker build -t pmng/apache2-php7 ./apache-php
 docker build -t pmng/nginx-php7 ./nginx-php
-docker build -t pmng/mariadb ./alpine-mariadb
+docker build -t pmng/plugin-mariadb ./alpine-mariadb
