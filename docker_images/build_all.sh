28f05afc803c437d21c9d0ef955bf9f232293fe0
OriginalsByToto <thomas.ledos92@gmail.com>
2020-08-02 20:34:51 +0200

    Corrected docker base build folder

--- docker_images/build_all.sh
+++ docker_images/build_all.sh
@@ -1,4 +1,4 @@
-docker build -t pmng/base ./base-project
+docker build -t pmng/base ./base
 docker build -t pmng/node -t pmng/node:14.7.0 ./node/v14
 docker build -t pmng/node:13.14.0 ./node/v13
 docker build -t pmng/node:12.18.3 ./node/v12
