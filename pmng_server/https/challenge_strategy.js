1289cc804062146dd9c205d39422af1481100a1b
OriginalsByToto <thomas.ledos92@gmail.com>
2020-08-07 21:18:21 +0200

    Corrected Greenlock errors throwing

--- pmng_server/https/challenge_strategy.js
+++ pmng_server/https/challenge_strategy.js
@@ -14,21 +14,20 @@ module.exports.create = function () {
 
     m.set = function(opts) {
         return intercom.sendPromise("dnsChallenges", {command: "set", host: opts.challenge.dnsHost, token: opts.challenge.dnsAuthorization}).then((response) => {
-            if(response.error) throw new Error("Cannot set DNS challenge.");
+            if(response.error) throw "Cannot set DNS challenge.";
         });
     }
 
     m.get = function(opts) {
-        console.log("getting", opts.challenge);
         return intercom.sendPromise("dnsChallenge", {command: "get", host: opts.challenge.dnsHost}).then((response) => {
-            if(response.error) throw new Error("Cannot get DNS challenge.");
+            if(response.error) throw "Cannot get DNS challenge.";
             else return response.token;
         });
     }
 
     m.remove = function(opts) {
         return intercom.sendPromise("dnsChallenges", {command: "remove", host: opts.challenge.dnsHost, token: opts.challenge.dnsAuthorization}).then((response) => {
-            if(response.error) throw new Error("Cannot remove DNS challenge.");
+            if(response.error) throw "Cannot remove DNS challenge.";
         });
     }
 
