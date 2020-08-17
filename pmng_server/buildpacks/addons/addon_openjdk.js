079423d2975571487a028ecd55d5ef626ec2c420
OriginalsByToto <thomas.ledos92@gmail.com>
2020-08-17 18:41:56 +0200

    Addon OpenJDK deprecated v7

--- pmng_server/buildpacks/addons/addon_openjdk.js
+++ pmng_server/buildpacks/addons/addon_openjdk.js
@@ -8,6 +8,11 @@ class OpenJDKAddon extends BuildAddon {
         if(!(["7", "8", "9", "10", "11"].includes(jdkVersion))) {
             throw "Invalid OpenJDK version. Only versions 7 to 11 are supported (with OpenJDK 7.0 being deprecated).";
         } else {
+            if(jdkVersion == "7") {
+                logger("WARNING: You are using a deprecated version of OpenJDK. Please switch ASAP to a more recent version.");
+                logger("Versions from 8 through 11 are officially supported and maintained.\n");
+            }
+
             logger("Installing OpenJDK (please wait, it might take a moment)...");
             let resp = await utils.execCommand("apk add --no-cache openjdk" + jdkVersion + "-jre", undefined, false, "root");
             if(resp.err.trim().length > 0) throw resp.err;
