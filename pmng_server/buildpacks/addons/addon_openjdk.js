5353dbefe26486a74b70a3753540b8f518ca6433
OriginalsByToto <thomas.ledos92@gmail.com>
2020-08-17 18:41:42 +0200

    Added more OpenJDK addon versions (7, 9 and 10)

--- pmng_server/buildpacks/addons/addon_openjdk.js
+++ pmng_server/buildpacks/addons/addon_openjdk.js
@@ -5,8 +5,8 @@ class OpenJDKAddon extends BuildAddon {
         let jdkVersion = (addonData.version || "latest").toString();
         if(jdkVersion == "latest") jdkVersion = "11";
 
-        if(!(["8", "11"].includes(jdkVersion))) {
-            throw "Invalid OpenJDK version. Only versions 8 and 11 are supported.";
+        if(!(["7", "8", "9", "10", "11"].includes(jdkVersion))) {
+            throw "Invalid OpenJDK version. Only versions 7 to 11 are supported (with OpenJDK 7.0 being deprecated).";
         } else {
             logger("Installing OpenJDK (please wait, it might take a moment)...");
             let resp = await utils.execCommand("apk add --no-cache openjdk" + jdkVersion + "-jre", undefined, false, "root");
