4f913b11ccaf38b7af3b5de990c901fcd75fabf1
Thomas <thomas.ledos92@gmail.com>
2020-08-24 13:13:23 +0200

    Corrected buildtools addon

--- pmng_server/buildpacks/addons/addon_buildtools.js
+++ pmng_server/buildpacks/addons/addon_buildtools.js
@@ -1,31 +1,21 @@
 const BuildAddon = require("./lib_addon");
-const allowed_packages = {
-    "make": "make",
-    "gcc": "gcc"
-};
 
 class BuildToolsAddon extends BuildAddon {
     static async addon(projectName, addonData, utils, logger) {
-        let pythonVersion = (addonData.version || "latest").toString();
-        if(pythonVersion == "latest") pythonVersion = "3";
+        let allowed_packages = {
+            "make": "make",
+            "gcc": "gcc",
+            "g++": "g++"
+        };        
 
-        if(!(["2", "3"].includes(pythonVersion))) {
-            throw "Invalid Python version. Builds only support python2 and python3.";
-        } else {
-            logger("Installing python" + pythonVersion + " (please wait, it might take a moment)...");
-            let resp = await utils.execCommand("apk add --no-cache python" + pythonVersion, undefined, false, "root");
-            if(resp.err.trim().length > 0) throw resp.err;
-            logger("python" + pythonVersion + " installed.");
-        }
-        
         let tools = addonData.tools || [];
         if(tools.length == 0) logger("No build tool to install.");
         else {
             for(let tool of tools) {
-                let package = allowed_packages[tool];
-                if(package != undefined) {
-                    logger("Installing build tool " + tool + (package == tool ? "..." : " (package " + package + ")..."));
-                    let resp = await utils.execCommand("apk add --no-cache " + package, undefined, false, "root");
+                let alpinePkg = allowed_packages[tool];
+                if(alpinePkg != undefined) {
+                    logger("Installing build tool " + tool + (alpinePkg == tool ? "..." : " (package " + alpinePkg + ")..."));
+                    let resp = await utils.execCommand("apk add --no-cache " + alpinePkg, undefined, false, "root");
                     if(resp.err.trim().length > 0) throw resp.err;
                 } else throw "Tool '" + tool + "' doesn't exist. Cannot continue building addon.";
             }
@@ -35,7 +25,7 @@ class BuildToolsAddon extends BuildAddon {
     }
 
     static displayName() {
-        return "Python";
+        return "Build Tools";
     }
 }
 
