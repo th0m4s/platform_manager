14842d0e15e80ce4bf82855fd94de5da650051e7
Thomas LEDOS <thomas.ledos92@gmail.com>
2020-08-26 19:07:58 +0200

    Docker PHP images print port

--- docker_images/apache-php/entrypoint.sh
+++ docker_images/apache-php/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sed -i "s/Listen 80/Listen $PORT/g" /etc/apache2/httpd.conf
-echo "Starting server..."
+echo "Starting server on port $PORT..."
 httpd -DNO_DETACH
--- docker_images/nginx-php/entrypoint.sh
+++ docker_images/nginx-php/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sed -i "s/listening_port/$PORT/g" /etc/nginx/nginx.conf
-echo "Starting server..."
+echo "Starting server on port $PORT..."
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
