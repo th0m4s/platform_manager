5612557eef8f0f549d864106064f4493ed3f5940
Thomas LEDOS <thomas.ledos92@gmail.com>
2020-09-03 19:00:59 +0200

    Users API corrected function syntax

--- pmng_server/admin_panel/api_controllers/v1/users_api.js
+++ pmng_server/admin_panel/api_controllers/v1/users_api.js
@@ -2,7 +2,7 @@ const express = require('express'), router = express.Router();
 const database_server = require("../../../database_server");
 const api_auth = require("./api_auth");
 
-router.get("/exists/:username", function(req, res) {
+router.get("/exists/:username", (req, res) => {
     api_auth(req, res, function(user) {
         database_server.findUserId(req.params.username).then((id) => {
             res.status(200).json({error: false, code: 200, message: "User found", exists: true, id});
