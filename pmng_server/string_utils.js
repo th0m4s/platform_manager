1f693e0ea20942fdc2967d424b20a83ffc09dded
Thomas LEDOS <thomas.ledos92@gmail.com>
2020-09-06 14:06:43 +0200

    Corrected size prefixes

--- pmng_server/string_utils.js
+++ pmng_server/string_utils.js
@@ -20,7 +20,7 @@ function formatBytes(bytes, decimals = 2) {
 
     const k = 1024;
     const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    const sizes = ["B", "K", "M", "G", "T", "P", "E", "Z", "Y"];
 
     const i = Math.floor(Math.log(bytes) / Math.log(k));
 
