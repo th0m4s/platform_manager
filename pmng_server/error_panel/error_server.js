2b41e6d7922b2a5498ca0b75b009f879658d72c1
Thomas LEDOS <thomas.ledos92@gmail.com>
2020-10-04 17:02:23 +0200

    Added error server redirects

--- pmng_server/error_panel/error_server.js
+++ pmng_server/error_panel/error_server.js
@@ -7,6 +7,18 @@ const privileges = require("../privileges");
 const stylePath = path.join(__dirname, "static/style.css");
 app.get("/style.css", (req, res) => {
     res.sendFile(stylePath);
+});
+
+const errorRedirects = {mails: "__HTTPSadmin.__ROOT_DOMAIN/webmail"};
+app.get("/", (req, res, next) => {
+    let host = req.headers.host;
+    if(host.endsWith(process.env.ROOT_DOMAIN)) {
+        let redType = host.substring(0, host.length-process.env.ROOT_DOMAIN.length-1);
+        let redirect = errorRedirects[redType];
+        if(redirect != undefined) {
+            res.redirect(redirect.replace(/__ROOT_DOMAIN/g, process.env.ROOT_DOMAIN).replace(/__HTTPS/g, "http" + (process.env.ENABLE_HTTPS.toLowerCase() == "true" ? "s": "") + "://"));
+        } else next();
+    } else next();
 })
 
 const errorPage = path.join(__dirname, "static/port_error.html");
