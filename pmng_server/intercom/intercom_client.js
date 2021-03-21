4057d573cfe824f176477b8de5fa4ce860fb2a8b
Thomas LEDOS <thomas.ledos92@gmail.com>
2021-03-21 20:44:26 +0100

    Added reconnection to intercom client

--- pmng_server/intercom/intercom_client.js
+++ pmng_server/intercom/intercom_client.js
@@ -1,8 +1,8 @@
 const net = require("net");
 
 /**
- * @typedef {{subscribe: (subjects: string[], callback: (message: object, respond: (response: object) => void) => void) => void,
- *  send: (subject: string, message: object, responseCallback: (response: object) => void) => void,
+ * @typedef {{subscribe: (subjects: string[], callback: (message: object, respond: (response: object) => void) => void) => boolean,
+ *  send: (subject: string, message: object, responseCallback: (response: object) => void) => boolean,
  *  sendPromise: (subject: string, message: object) => Promise<object>,
  *  changeConfig: (newConfig: {timeout?: number, autoReject?: boolean, autoResolve?: boolean}) => void}} IntercomClient 
  */
@@ -13,44 +13,71 @@ const net = require("net");
  * @returns {IntercomClient} The connected intercom client.
  */
 function connect() {
-    let connection = net.createConnection(8043);
+    let internalConnect = () => {
+        let conn = net.createConnection(8043);
+        conn.on("data", (buffer) => {
+            for(let c of buffer.toString("utf-8")) {
+                if(c == "\n") {
+                    let currentMessage = rcvBuffer;
+                    rcvBuffer = "";
+    
+                    let command = currentMessage.substring(0, 4);
+                    let value = JSON.parse(currentMessage.substring(5, currentMessage.length));
+    
+                    processCommand(conn, command, value, subs, waitingResp);
+                } else rcvBuffer += c;
+            }
+        });
+
+        return conn;
+    }
+
+    let connection = internalConnect();
     let subs = {}, waitingResp = {};
     let defaultConfig = {timeout: 20, autoReject: true, autoResolve: true};
 
     let rcvBuffer = "";
-    connection.on("data", (buffer) => {
-        for(let c of buffer.toString("utf-8")) {
-            if(c == "\n") {
-                let currentMessage = rcvBuffer;
-                rcvBuffer = "";
 
-                let command = currentMessage.substring(0, 4);
-                let value = JSON.parse(currentMessage.substring(5, currentMessage.length));
+    let checkConnection = () => {
+        try {
+            if(connection == undefined || connection.destroyed)
+                connection = internalConnect();
 
-                processCommand(connection, command, value, subs, waitingResp);
-            } else rcvBuffer += c;
+            return true;
+        } catch(_) {
+            return false;
         }
-    });
+    }
 
     return {
         subscribe: function(subjects, callback) {
+            if(!checkConnection()) return false;
+
             connection.write("subs:" + JSON.stringify(subjects) + "\n");
             subjects.forEach((subject) => {
                 if(!subs.hasOwnProperty(subject)) subs[subject] = [];
                 subs[subject].push(callback);
             });
+
+            return true;
         },
         send: function(subject, message, responseCallback) {
+            if(!checkConnection()) return false;
+
             let id = Math.floor(Math.random()*((2**32)-1));
             if(responseCallback != undefined) waitingResp[id] = responseCallback;
             connection.write("send:" + JSON.stringify({subject: subject, message: message, id: id}) + "\n");
+
+            return true;
         },
         sendPromise: function(subject, message, {timeout = defaultConfig.timeout, autoReject = defaultConfig.autoReject, autoResolve = defaultConfig.autoResolve} = {}) {
             let respProm = new Promise((resolve, reject) => {
-                this.send(subject, message, (response) => {
+                let sent = this.send(subject, message, (response) => {
                     if(autoReject && response.error !== undefined && response.error === true) return reject(response.message);
                     else return resolve(autoResolve ? (response.message || response) : response);
                 });
+
+                if(!sent) reject("No connection available!");
             });
             return timeout > 0 ? Promise.race([new Promise((resolve, reject) => {
                 setTimeout(() => {
