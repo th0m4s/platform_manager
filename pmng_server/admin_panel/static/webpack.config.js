152f92f4c4914b8b2cc83887b09f7fc9753d9043
Thomas LEDOS <thomas.ledos92@gmail.com>
2021-05-07 19:54:55 +0200

    Updated webpack module ids

--- pmng_server/admin_panel/static/webpack.config.js
+++ pmng_server/admin_panel/static/webpack.config.js
@@ -1,42 +1,44 @@
-const webpack = require("webpack");
 const path = require("path");
 const glob = require("glob");
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 
 let config = {
     mode: "production",
     entry: glob.sync(path.join(__dirname, "src", "**.js")).reduce(function(obj, el) {
-      obj[path.parse(el).name] = el;
-      return obj
+        obj[path.parse(el).name] = el;
+        return obj
     }, {}),
     externals: [
-      function(context, request, callback) {
-        if (/^moment$/.test(request) && context.includes("node_modules")){
-          return callback(null, 'root ' + request);
+        function(context, request, callback) {
+            if (/^moment$/.test(request) && context.includes("node_modules")){
+                return callback(null, "root " + request);
+            }
+    
+            callback();
         }
-  
-        callback();
-      }
     ],
+    optimization: {
+        moduleIds: "hashed" 
+    },
     output: {
-      path: path.join(__dirname, "dist", "js"),
-      filename: "./[name].dist.js"
+        path: path.join(__dirname, "dist", "js"),
+        filename: "./[name].dist.js"
     },
     module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        }
-      ]
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
     },
     performance : {
-      hints : false
+        hints : false
     },
     plugins: [
-      new MomentLocalesPlugin()
+        new MomentLocalesPlugin()
     ]
     // still displays warning about locales.....
-  }
+}
   
-  module.exports = config;
+module.exports = config;
