866ca1d1bb0f00e598bcd7f6ae96015b30d356fb
Thomas LEDOS <thomas.ledos92@gmail.com>
2021-10-16 23:11:30 +0200

    Added dotnet appbinary

--- pmng_server/buildpacks/addons/addon_appbinary.js
+++ pmng_server/buildpacks/addons/addon_appbinary.js
@@ -14,7 +14,7 @@ async function bentFollowRedirect(url, maxRedirect = 3) {
             let tempReq = await bentObj(url);
             req = tempReq;
         } catch(e) {
-            if(e.statusCode == 302) {
+            if(e.statusCode == 302 || e.statusCode == 301) {
                 url = e.headers.location;
             } else throw e;
         }
@@ -34,14 +34,20 @@ class AppBinary extends BuildAddon {
         //let bentAdmin = bent("http" + (process.env.ENABLE_HTTPS.toLowerCase() == "true" ? "s" : "") + "://admin." + process.env.ROOT_DOMAIN + "/");
 
         for(let app of apps) {
+            let appData = {};
+            if(typeof app === "object") {
+                appData = app;
+                app = appData.name;
+            }
+
             switch(app) {
                 case "phantomjs":
-                    let filename = "dockerized-phantomjs.tar.gz";
+                    let phantomFilename = "dockerized-phantomjs.tar.gz";
     
                     logger("Downloading PhantomJS...");
-                    let downloadPaths = utils.temporaryFile(filename);
+                    let phantomDownloadPaths = utils.temporaryFile(phantomFilename);
                     try {
-                        let writeStream = fs.createWriteStream(downloadPaths.host);
+                        let writeStream = fs.createWriteStream(phantomDownloadPaths.host);
                         let downloadStream = await bentFollowRedirect("https://github.com/fgrehm/docker-phantomjs2/releases/download/v2.0.0-20150722/dockerized-phantomjs.tar.gz");
 
                         downloadStream.pipe(writeStream);
@@ -55,15 +61,49 @@ class AppBinary extends BuildAddon {
                     }
     
                     logger("Extracting archive...");
-                    let tempDir = path.dirname(downloadPaths.container);
-                    let extractExec = await utils.execCommand("tar -xzf " + filename + " -C /tmp/ && cp -R /tmp/etc/fonts /etc/ && cp -R /tmp/lib/* /lib/ && cp -R /tmp/lib64 / && cp -R /tmp/usr/lib/* /usr/lib/ && cp -R /tmp/usr/lib/x86_64-linux-gnu /usr/ && cp -R /tmp/usr/share/* /usr/share/ && cp /tmp/usr/local/bin/phantomjs /usr/bin/ && rm -rf /tmp/*", undefined, false, "root", tempDir);
+                    let phantomTempDir = path.dirname(phantomDownloadPaths.container);
+                    let extractExec = await utils.execCommand("tar -xzf " + phantomFilename + " -C /tmp/ && cp -R /tmp/etc/fonts /etc/ && cp -R /tmp/lib/* /lib/ && cp -R /tmp/lib64 / && cp -R /tmp/usr/lib/* /usr/lib/ && cp -R /tmp/usr/lib/x86_64-linux-gnu /usr/ && cp -R /tmp/usr/share/* /usr/share/ && cp /tmp/usr/local/bin/phantomjs /usr/bin/ && rm -rf /tmp/*", undefined, false, "root", phantomTempDir);
                     if(extractExec.code != 0)
                         throw "Cannot extract PhantomJS archive: " + extractExec.err;
                     logger("PhantomJS files added to the project.");
 
-                    await pfs.unlink(downloadPaths.host);
+                    await pfs.unlink(phantomDownloadPaths.host);
                     logger("Temporary files removed. App installation done.");
 
+                    break;
+                case "dotnet":
+                    let dotnetFilename = "dotnet-install.sh";
+
+                    logger("Installing required librairies...");
+                    let resp = await utils.execCommand("apk add --no-cache icu-libs krb5-libs libgcc libintl libssl1.1 libstdc++ zlib", undefined, false, "root");
+                    if(resp.err.trim().length > 0) throw resp.err;
+    
+                    logger("Downloading .NET install script...");
+                    let dotnetDownloadPaths = utils.temporaryFile(dotnetFilename);
+                    try {
+                        let writeStream = fs.createWriteStream(dotnetDownloadPaths.host);
+                        let downloadStream = await bentFollowRedirect("https://dot.net/v1/dotnet-install.sh");
+
+                        downloadStream.pipe(writeStream);
+    
+                        await new Promise((resolve, reject) => {
+                            downloadStream.on("error", reject);
+                            downloadStream.on("end", resolve);
+                        });
+                    } catch(error) {
+                        throw "Cannot download install script: " + error;
+                    }
+
+                    logger("Installing .NET from script...");
+                    let dotnetTempDir = path.dirname(dotnetDownloadPaths.container);
+                    let installExec = await utils.execCommand("chmod +x " + dotnetFilename + " && ./" + dotnetFilename + " --install-dir /usr/share/dotnet" + (appData.channel != undefined ? (" -c " + appData.channel) : "") + (appData.version != undefined ? (" -v " + appData.version) : ""), undefined, false, "root", dotnetTempDir);
+                    if(installExec.code != 0)
+                        throw "Cannot install .NET: " + installExec.err;
+
+                    logger("Installing dotnet-script...");
+                    let dotnetScriptExec = await utils.execCommand("/usr/share/dotnet/dotnet tool install -g dotnet-script", undefined, false, "root", dotnetTempDir);
+                    if(dotnetScriptExec.code != 0)
+                        throw "Cannot install dotnet-script: " + dotnetScriptExec.err;
                     break;
                 default:
                     throw "Invalid app provided. Cannot build addon.";
