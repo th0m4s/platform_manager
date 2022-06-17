7a62090174a2934e6eca3c6948767f9b69ccbb5b
Thomas LEDOS <thomas.ledos92@gmail.com>
2022-06-17 21:07:53 +0200

    Added logs to git versioning reading

--- git_versioning.js
+++ git_versioning.js
@@ -1,8 +1,11 @@
 const child_process = require("child_process");
+const logger = require("./pmng_server/platform_logger").logger();
 
 module.exports.readGitInfo = (disableReject = false) => {
     return new Promise((resolve, reject) => {
         if(process.env.PMNG_GIT_TAG != undefined && process.env.PMNG_GIT_COMMIT != undefined) {
+            logger.tagInfo("GIT", "Git information already loaded.");
+            logger.tagInfo("GIT", process.env.PMNG_GIT_COMMIT + " (" + process.env.PMNG_GIT_TAG + ")");
             resolve();
         } else {
             child_process.exec("git log -1 --format=oneline", (error, stdout, stderr) => {
@@ -18,10 +21,15 @@ module.exports.readGitInfo = (disableReject = false) => {
 
                         if(logTag != undefined) {
                             process.env.PMNG_GIT_TAG = logTag;
+
+                            logger.tagInfo("GIT", process.env.PMNG_GIT_COMMIT + " (" + process.env.PMNG_GIT_TAG + ")");
                             resolve();
                         } else {
+                            logger.tagInfo("GIT", "Using 'git describe --tags' to read tag info...");
                             child_process.exec("git describe --tags", (tagError, tagStdout, tagStderr) => {
                                 process.env.PMNG_GIT_TAG = tagError == null ? tagStdout.trim().split("-")[0] : "unknown";
+
+                                logger.tagInfo("GIT", process.env.PMNG_GIT_COMMIT + " (" + process.env.PMNG_GIT_TAG + ")");
                                 resolve();
                             });
                         }
@@ -31,5 +39,6 @@ module.exports.readGitInfo = (disableReject = false) => {
         }
     }).catch((error) => {
         if(!disableReject) throw error;
+        else logger.tagError("GIT", "Cannot get last commit log!" + (error.message ?? error));
     });
 };
