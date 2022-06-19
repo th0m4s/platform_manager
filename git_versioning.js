08dbbc03131567eec298f7772a371455b3dc19c4
Thomas LEDOS <thomas.ledos92@gmail.com>
2022-06-19 19:46:32 +0200

    Corrected git versionning reading as root

--- git_versioning.js
+++ git_versioning.js
@@ -8,37 +8,40 @@ module.exports.readGitInfo = (disableReject = false) => {
             logger.tagInfo("GIT", process.env.PMNG_GIT_COMMIT + " (" + process.env.PMNG_GIT_TAG + ")");
             resolve();
         } else {
-            child_process.exec("git log -1 --format=oneline", (error, stdout, stderr) => {
-                if(error) reject(error);
-                else {
-                    let logRegex = /^(?<commit>[a-f0-9]{40}) (?:\((?:tag: (?<tag>[^,]+)[^)]*|)\) |).*$/gm;
-                    let logResult = logRegex.exec(stdout.trim());
-
-                    if(logResult == null) reject(new Error("Cannot get last commit log!"));
+            child_process.exec("git config --global --add safe.directory " + process.cwd(), (safeError, safeStdout, safeStderr) => {
+                if(safeError) reject(safeError);
+                else child_process.exec("git log -1 --format=oneline", (error, stdout, stderr) => {
+                    if(error) reject(error);
                     else {
-                        process.env.PMNG_GIT_COMMIT = logResult.groups["commit"];
-                        let logTag = logResult.groups["tag"];
-
-                        if(logTag != undefined) {
-                            process.env.PMNG_GIT_TAG = logTag;
-
-                            logger.tagInfo("GIT", process.env.PMNG_GIT_COMMIT + " (" + process.env.PMNG_GIT_TAG + ")");
-                            resolve();
-                        } else {
-                            logger.tagInfo("GIT", "Using 'git describe --tags' to read tag info...");
-                            child_process.exec("git describe --tags", (tagError, tagStdout, tagStderr) => {
-                                process.env.PMNG_GIT_TAG = tagError == null ? tagStdout.trim().split("-")[0] : "unknown";
-
+                        let logRegex = /^(?<commit>[a-f0-9]{40}) (?:\((?:tag: (?<tag>[^,]+)[^)]*|)\) |).*$/gm;
+                        let logResult = logRegex.exec(stdout.trim());
+    
+                        if(logResult == null) reject(new Error("Cannot get last commit log!"));
+                        else {
+                            process.env.PMNG_GIT_COMMIT = logResult.groups["commit"];
+                            let logTag = logResult.groups["tag"];
+    
+                            if(logTag != undefined) {
+                                process.env.PMNG_GIT_TAG = logTag;
+    
                                 logger.tagInfo("GIT", process.env.PMNG_GIT_COMMIT + " (" + process.env.PMNG_GIT_TAG + ")");
                                 resolve();
-                            });
+                            } else {
+                                logger.tagInfo("GIT", "Using 'git describe --tags' to read tag info...");
+                                child_process.exec("git describe --tags", (tagError, tagStdout, tagStderr) => {
+                                    process.env.PMNG_GIT_TAG = tagError == null ? tagStdout.trim().split("-")[0] : "unknown";
+    
+                                    logger.tagInfo("GIT", process.env.PMNG_GIT_COMMIT + " (" + process.env.PMNG_GIT_TAG + ")");
+                                    resolve();
+                                });
+                            }
                         }
                     }
-                }
+                });
             });
         }
     }).catch((error) => {
         if(!disableReject) throw error;
-        else logger.tagError("GIT", "Cannot get last commit log!" + (error.message ?? error));
+        else logger.tagError("GIT", "Cannot get last commit log! " + (error.message ?? error));
     });
 };
